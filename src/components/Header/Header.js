ff3093f40fa5a27956cd138daf71a23a85aaec55
willcab <willianeduardocg13@gmail.com>
2022-05-18 01:45:26 -0500

    components reusable

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+const Header = ({logo}) => (
+	<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<p>
+			Edit <code>src/App.js</code> and save to reload.
+		</p>
+		<a
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Learn React
+		</a>
+	</header>
+);
+export default Header;
