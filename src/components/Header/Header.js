8fa985a9fe954886cafd12dfd7de2440988d5ff6
willcab <willianeduardocg13@gmail.com>
2022-05-18 02:04:55 -0500

    split on components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./Header.css";
+import Link from "../Link/Link";
 const Header = ({logo}) => (
 	<header className="App-header">
 		<img src={logo} className="App-logo" alt="logo" />
 		<p>
 			Edit <code>src/App.js</code> and save to reload.
 		</p>
-		<a
-			className="App-link"
-			href="https://reactjs.org"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			Learn React
-		</a>
+		<Link url='https://reactjs.org' title='Learn React'/>
 	</header>
 );
 export default Header;
