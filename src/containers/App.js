528013393b92b8fc0dbb7f8184648bf2560db3bf
willcab <willianeduardocg13@gmail.com>
2022-05-18 21:55:34 -0500

    style global add, delete index.css

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,33 @@
 import React, { Component, Profiler } from "react";
 import Header from "../components/Header/Header";
-import Profile from "./Profile"
-import styledComponents from "styled-components";
+import Profile from "./Profile";
+import styledComponents, { createGlobalStyle } from "styled-components";
 
-const AppWrapper= styledComponents.div`
+const GlobalStyle = createGlobalStyle`
+	body{
+		margin: 0;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+		  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+		  sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+`;
+
+const AppWrapper = styledComponents.div`
 	text-aling:center;
 `;
 
 class App extends Component {
 	render() {
 		return (
-			<AppWrapper>
-			 <Header/>
-			 <Profile/>
-			</AppWrapper>
+			<>
+				<GlobalStyle />
+				<AppWrapper>
+					<Header />
+					<Profile />
+				</AppWrapper>
+			</>
 		);
 	}
 }
